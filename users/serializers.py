d36185552f8f7cf0d13fbf5d36c49bce924bc551
VitaliyKladko <vitaliykladko13@gmail.com>
2023-05-12 22:21:22 +0300

    add UpdateAPIView + UpdateAPIView serializers for User model

--- users/serializers.py
+++ users/serializers.py
@@ -60,3 +60,31 @@ class UserCreateAPIViewSerializer(serializers.ModelSerializer):
             user.locations.add(loc)
         user.save()
         return user
+
+
+class UserUpdateAPIViewSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    locations = serializers.SlugRelatedField(
+        required=False,
+        many=True,
+        queryset=Location.objects.all(),
+        slug_field='name'
+    )
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def is_valid(self, *, raise_exception=False):
+        self._location = self.initial_data.pop('locations')
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self, **kwargs):
+        user = super().save()
+
+        for loc_name in self._location:
+            loc_obj, _ = Location.objects.get_or_create(name=loc_name)
+            user.locations.clear()
+            user.locations.add(loc_obj)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -6,11 +6,12 @@ from django.http import JsonResponse, Http404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 
 from hw_28_v3 import settings
 from users.models import User, Location
-from users.serializers import UserListViewSerializer, UserRetrieveViewSerializer, UserCreateAPIViewSerializer
+from users.serializers import UserListViewSerializer, UserRetrieveViewSerializer, UserCreateAPIViewSerializer, \
+    UserUpdateAPIViewSerializer
 
 
 class UserListView(ListAPIView):
@@ -26,6 +27,11 @@ class UserDetailView(RetrieveAPIView):
 class UserCreateView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateAPIViewSerializer
+
+
+class UserUpdateView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateAPIViewSerializer
 
 
 # class UserListView(ListView):
@@ -77,41 +83,41 @@ class UserCreateView(CreateAPIView):
 #         return JsonResponse(user.serialize(), status=201)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class UserUpdateView(UpdateView):
-    model = User
-    fields = ['username', 'password', 'first_name', 'last_name', 'age', 'locations']
-
-    def patch(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
-
-        user_update_data = json.loads(request.body)
-
-        if 'username' in user_update_data:
-            self.object.username = user_update_data['username']
-
-        if 'password' in user_update_data:
-            self.object.password = user_update_data['password']
-
-        if 'first_name' in user_update_data:
-            self.object.first_name = user_update_data['first_name']
-
-        if 'last_name' in user_update_data:
-            self.object.last_name = user_update_data['last_name']
-
-        if 'age' in user_update_data:
-            self.object.age = user_update_data['age']
-
-        if 'locations' in user_update_data:
-            locations = user_update_data.pop('locations')
-            self.object.locations.clear()  # метод очистит связи между User и Location
-            for loc_name in locations:
-                loc, _ = Location.objects.get_or_create(name=loc_name)
-                self.object.locations.add(loc)
-
-        self.object.save()
-
-        return JsonResponse(self.object.serialize(), status=200)
+# @method_decorator(csrf_exempt, name='dispatch')
+# class UserUpdateView(UpdateView):
+#     model = User
+#     fields = ['username', 'password', 'first_name', 'last_name', 'age', 'locations']
+#
+#     def patch(self, request, *args, **kwargs):
+#         super().post(request, *args, **kwargs)
+#
+#         user_update_data = json.loads(request.body)
+#
+#         if 'username' in user_update_data:
+#             self.object.username = user_update_data['username']
+#
+#         if 'password' in user_update_data:
+#             self.object.password = user_update_data['password']
+#
+#         if 'first_name' in user_update_data:
+#             self.object.first_name = user_update_data['first_name']
+#
+#         if 'last_name' in user_update_data:
+#             self.object.last_name = user_update_data['last_name']
+#
+#         if 'age' in user_update_data:
+#             self.object.age = user_update_data['age']
+#
+#         if 'locations' in user_update_data:
+#             locations = user_update_data.pop('locations')
+#             self.object.locations.clear()  # метод очистит связи между User и Location
+#             for loc_name in locations:
+#                 loc, _ = Location.objects.get_or_create(name=loc_name)
+#                 self.object.locations.add(loc)
+#
+#         self.object.save()
+#
+#         return JsonResponse(self.object.serialize(), status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
