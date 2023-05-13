3b1db64273f3b9d33386a66b4c53773147afe45a
VitaliyKladko <vitaliykladko13@gmail.com>
2023-05-13 13:51:13 +0300

    clear users.views.py import

--- users/views.py
+++ users/views.py
@@ -1,15 +1,6 @@
-import json
-
-from django.core.paginator import Paginator
-from django.db.models import Count, Q
-from django.http import JsonResponse, Http404
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
-from hw_28_v3 import settings
 from users.models import User, Location
 from users.serializers import UserListViewSerializer, UserRetrieveViewSerializer, UserCreateAPIViewSerializer, \
     UserUpdateAPIViewSerializer, UserDestroyAPIViewSerializer, LocationSerializer
